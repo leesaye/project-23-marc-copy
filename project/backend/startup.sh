2a21b4ab4da70e6c4ef572f52766b370dbf54189
HumrajB <humrajbhoday@gmail.com>
2025-02-07 01:03:40 -0500

    add django rest api backend and implement jwt cookie authentication

---
+++ project/backend/startup.sh
@@ -0,0 +1,11 @@
+virtualenv ../venv
+source ../venv/bin/activate
+
+#django package installs
+python -m pip install djangorestframework
+python -m pip install djangorestframework-simplejwt
+python -m pip install django-cors-headers
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
