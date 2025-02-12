9888933befa05fd6881db030e692f18807ed9268
HumrajB <humrajbhoday@gmail.com>
2025-02-11 21:52:36 -0500

    Move AuthProvider to App.js

--- project/frontend/personal-crm/src/components/Main.js
+++ project/frontend/personal-crm/src/components/Main.js
@@ -11,15 +11,13 @@ import { AuthProvider } from '../contexts/useAuth';
 function Main() {
     return (
         <Fragment>
-            <AuthProvider>
-                <Routes>
-                    {/* Add routes to each different pages with their link path here */}
-                    <Route path ='/login' element={<Login />} />
-                    <Route path ='/register' element={<Register />} />
-                    <Route path='/' element={<PrivateRoute><Home/></PrivateRoute>} />
-                    <Route path='/contacts/' element={<PrivateRoute><Contacts/></PrivateRoute>} />
-                </Routes>
-            </AuthProvider>
+            <Routes>
+                {/* Add routes to each different pages with their link path here */}
+                <Route path ='/login' element={<Login />} />
+                <Route path ='/register' element={<Register />} />
+                <Route path='/' element={<PrivateRoute><Home/></PrivateRoute>} />
+                <Route path='/contacts/' element={<PrivateRoute><Contacts/></PrivateRoute>} />
+            </Routes>
         </Fragment>
     );
 }
