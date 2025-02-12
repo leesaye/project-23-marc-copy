d359dca68320a4334b4dd50b8bc78b7be75bc697
HumrajB <humrajbhoday@gmail.com>
2025-02-11 21:32:48 -0500

    Add Logout to sidebar and sync main into sidebranch

--- project/frontend/personal-crm/src/pages/Home.js
+++ project/frontend/personal-crm/src/pages/Home.js
@@ -1,22 +1,9 @@
 import Layout from "../components/Layout";
 
-import { useNavigate } from "react-router-dom"
-
-import { logout } from "../endpoints/api";
 function Home() {
-    const nav = useNavigate();
-    const handleLogout = async () => {
-        const success = await logout();
-        if (success){
-            nav('/login')
-        }
-    }
     return (
         <Layout>
             <h1>Home</h1>
-            <button className="btn btn-primary w-100 fs-6" onClick={()=>handleLogout()}>
-                Logout
-            </button>
         </Layout>
     );
 }
