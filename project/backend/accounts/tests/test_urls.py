639d48f0660f6b8922096065c173812856d3dca3
HumrajB <humrajbhoday@gmail.com>
2025-02-13 21:14:28 -0500

    Add authentication API testcases

---
+++ project/backend/accounts/tests/test_urls.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.urls import reverse
+
+class AuthenticationTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.login_url = reverse("token_obtain_pair")
+        self.refresh_url = reverse("token_refresh")
+        self.logout_url = reverse("logout")
+        self.authenticated_url = reverse("is_authenticated")
+        self.register_url = reverse("register")
+
+    def test_register_user(self):
+        data = {"username": "newuser", "email": "new@email.com", "password": 123}
+        response = self.client.post(self.register_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(User.objects.filter(username="newuser").exists())
+
+    def test_obtain_token(self):
+        data = {"username": "testuser", "password": "testpassword"}
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access_token", response.cookies)
+        self.assertIn("refresh_token", response.cookies)
+
+    def test_refresh_token(self):
+        refresh = RefreshToken.for_user(self.user)
+        self.client.cookies["refresh_token"] = str(refresh)
+        response = self.client.post(self.refresh_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access_token", response.cookies) 
+
+    def test_logout_logged_in(self):
+        data = {"username": "testuser", "password": "testpassword"}
+        response = self.client.post(self.login_url, data)
+        self.client.cookies = response.cookies
+
+        response = self.client.post(self.logout_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        
+    def test_authenticated_access(self):
+        data = {"username": "testuser", "password": "testpassword"}
+        response = self.client.post(self.login_url, data)
+        self.client.cookies = response.cookies
+
+        response = self.client.post(self.authenticated_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'authenticated': True})
+    
+    def test_unauthenticated_access(self):
+        response = self.client.post(self.authenticated_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
