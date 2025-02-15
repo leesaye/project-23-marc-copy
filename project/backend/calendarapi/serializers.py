882205ff6c8d7b136ce69dbc6f01cddb768463b2
arifa <arifa.mansoor@mail.utoronto.ca>
2025-02-15 16:51:40 -0500

    syncing users with events and tasks

--- project/backend/calendarapi/serializers.py
+++ project/backend/calendarapi/serializers.py
@@ -4,9 +4,9 @@ from .models import Event, Task
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ['id', 'user', 'title', 'start', 'end', 'color']
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'user', 'title', 'date', 'color']
--- project/backend/calendarapi/views.py
+++ project/backend/calendarapi/views.py
@@ -1,12 +1,85 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied, NotFound
 from .models import Event, Task
 from .serializers import EventSerializer, TaskSerializer
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = EventSerializer
 
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class IndividualEventView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id, user=request.user)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def post(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id, user=request.user)
+        serializer = EventSerializer(event, data=request.data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteEventView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id, user=request.user)
+        event.delete()
+        return Response({"message": "Event deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class IndividualTaskView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, task_id):
+        task = get_object_or_404(Task, id=task_id, user=request.user)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, id=task_id, user=request.user)
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteTaskView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, task_id):
+        task = get_object_or_404(Task, id=task_id, user=request.user)
+        task.delete()
+        return Response({"message": "Task deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
