79ec966cffcf342a08c2243d7100e7ecd733448f
supriyavasanth <supriya.vasanthakumaran@mail.utoronto.ca>
2025-02-15 09:20:47 -0500

    Added colors to events in backend

--- project/backend/calendarapi/models.py
+++ project/backend/calendarapi/models.py
@@ -5,6 +5,7 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     start = models.DateTimeField()
     end = models.DateTimeField()
+    color = models.CharField(max_length=7, default='#2f61a1')
 
     def __str__(self):
         return self.title
@@ -12,6 +13,7 @@ class Event(models.Model):
 class Task(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    color = models.CharField(max_length=7, default='#014f86')
 
     def __str__(self):
         return self.title
