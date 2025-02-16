5a729805f21e5cce2b83eb766df3c021ed5cda31
JTP001 <j.tavarespitts@mail.utoronto.ca>
2025-02-15 22:01:15 -0500

    Added random motivational quote to Home page

--- project/frontend/personal-crm/src/pages/Home.js
+++ project/frontend/personal-crm/src/pages/Home.js
@@ -1,9 +1,33 @@
 import Layout from "../components/Layout";
+import React, { useState, useEffect } from "react";
+import { getRandom, getAll } from "@divyanshu013/inspirational-quotes";
 
 function Home() {
+    const [quote, setQuote] = useState([]);
+
+    useEffect(() => {
+        const quoteData = getRandom();
+        setQuote(quoteData);
+    }, []);
+
     return (
         <Layout>
-            <h1>Home</h1>
+            <div className="conatiner bg-primary-subtle rounded p-3 vh-100">
+                <div className="row">
+                    <div className="col-2">
+                        <h2>Home</h2>
+                    </div>
+                </div>
+                <div className="text-center mx-5">
+                    <h3 className="mb-4">Featured motivational quote:</h3>
+                    {quote && 
+                    <blockquote className="blockquote">
+                        <h4><em>{quote.quote}</em></h4>
+                        <p>{quote.author}</p>
+                    </blockquote>
+                    }
+                </div>
+            </div>
         </Layout>
     );
 }
