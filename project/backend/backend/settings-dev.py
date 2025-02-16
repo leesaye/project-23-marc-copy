c72bcf9e0045a5f80eef954edc1cc5cedba68da4
bhodayhu <bhodayhu@teach.cs.utoronto.ca>
2025-02-16 01:01:38 -0500

    Setup setting.py to use env vars

--- project/backend/backend/settings-dev.py
+++ project/backend/backend/settings-dev.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import dj_database_url
-import os
 
 from pathlib import Path
 
@@ -22,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-n66=i#x#*7t3u0j7d(arj-r$cwx2elhhgzce$aon&4=zi%tlpj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -109,8 +107,7 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- project/backend/startup.sh
+++ project/backend/startup.sh
@@ -6,6 +6,10 @@ python -m pip install djangorestframework
 python -m pip install djangorestframework-simplejwt
 python -m pip install django-cors-headers
 
+#deployment packages
+python -m pip install dj-database-url
+python -m pip install psycopg2-binary 
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
