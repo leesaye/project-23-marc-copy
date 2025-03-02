3a40b89926d4f22104b7d256fb166a37ca26c518
arifa <arifa.mansoor@mail.utoronto.ca>
2025-03-01 21:05:14 -0500

    added contacts to tasks in the backend

--- project/backend/calendarapi/models.py
+++ project/backend/calendarapi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 # Create your models here.
 class Event(models.Model):
@@ -7,7 +8,6 @@ class Event(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     color = models.CharField(max_length=7, default='#4A90E2')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events', default=1)
 
     def str(self):
         return self.title
@@ -17,6 +17,7 @@ class Task(models.Model):
     date = models.DateField()
     color = models.CharField(max_length=7, default='#014f86')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', default=1)
+    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, blank=True, null=True)
 
     def str(self):
         return self.title
