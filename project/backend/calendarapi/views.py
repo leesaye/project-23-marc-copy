80cf3ad3d6d2481308f6d01d1bff23cf798be44b
supriyavasanth <supriya.vasanthakumaran@mail.utoronto.ca>
2025-03-07 03:01:54 -0500

    merged correctly

--- project/backend/calendarapi/views.py
+++ project/backend/calendarapi/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied, NotFound
+from rest_framework.decorators import api_view
 from .models import Event, Task
 from .serializers import EventSerializer, TaskSerializer
 from django.contrib.auth.models import User
