f4f9209e695357b99acb8ee3a5d954cee6f160fb
lisa <lis.ye@mail.utoronto.ca>
2025-03-07 17:45:12 -0500

    Add api to end of Todo sentence

--- project/frontend/personal-crm/src/components/TodoSentence.js
+++ project/frontend/personal-crm/src/components/TodoSentence.js
@@ -3,7 +3,7 @@ import axiosInstance from "../endpoints/api";
 import { Context } from "../contexts/AIContext";
 
 //const BASE_URL = "http://127.0.0.1:8000/api/";
-const BASE_URL = `https://project-23-marc.onrender.com/`;
+const BASE_URL = `https://project-23-marc.onrender.com/api/`;
 const EVENT_URL = `${BASE_URL}events/`;
 const TASK_URL = `${BASE_URL}tasks/`;
 
