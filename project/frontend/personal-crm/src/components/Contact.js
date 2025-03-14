b0950dea10528c85fdd96dbd854337bfbccb2aac
JTP001 <j.tavarespitts@mail.utoronto.ca>
2025-03-13 22:52:53 -0400

    Added responsiveness to contacts page and sidebar

--- project/frontend/personal-crm/src/components/Contact.js
+++ project/frontend/personal-crm/src/components/Contact.js
@@ -1,33 +1,58 @@
 import React from 'react';
 import { LinearProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 function Contact({contact}) {
+    const isSmallScreen = useMediaQuery({ query: '(max-width: 1224px)' });
+
     return (
         <div className="d-flex flex-column">
-            <div className="row" key={contact.id}>
-                <hr />
-                <div className="col-4">
-                    <p>{contact.name}</p>
+            {/* For desktop screen size */}
+            {!isSmallScreen ? (
+                <div className="row" key={contact.id}>
+                    <hr />
+                    <div className="col-4">
+                        <p>{contact.name}</p>
+                    </div>
+                    <div className="col-2">
+                        <p>{contact.job}</p>
+                    </div>
+                    <div className="col-3">
+                        <LinearProgress className="w-75"
+                            variant="determinate"
+                            value={contact.relationship_rating}
+                            sx={{ height: 10, borderRadius: 5 }}
+                        />
+                    </div>
+                    <div className="col-2">
+                        <p>{contact.relationship}</p>
+                    </div>
+                    <div className="col-1">
+                        <Link className="link-primary link-underline-opacity-0" to={`/contacts/${contact.id}/`}>Edit</Link>
+                    </div>
                 </div>
-                <div className="col-2">
-                    <p>{contact.job}</p>
+            ) : 
+            {/* For ipad/phone screen (not smallest phone size) */}
+            (
+                <div className="row" key={contact.id}>
+                    <hr />
+                    <div className="col-4">
+                        <p>{contact.name}</p>
+                    </div>
+                    <div className="col-5">
+                        <LinearProgress className="w-75"
+                            variant="determinate"
+                            value={contact.relationship_rating}
+                            sx={{ height: 10, borderRadius: 5 }}
+                        />
+                    </div>
+                    <div className="col-3">
+                        <Link className="link-primary link-underline-opacity-0" to={`/contacts/${contact.id}/`}>Edit</Link>
+                    </div>
                 </div>
-                <div className="col-3">
-                    <LinearProgress className="w-75"
-                        variant="determinate"
-                        value={contact.relationship_rating}
-                        sx={{ height: 10, borderRadius: 5 }}
-                    />
-                </div>
-                <div className="col-2">
-                    <p>{contact.relationship}</p>
-                </div>
-                <div className="col-1">
-                    <Link className="link-primary link-underline-opacity-0" to={`/contacts/${contact.id}/`}>Edit</Link>
-                </div>
-            </div>
-        </div>
+            )}
+        </div> 
     );
 }
 
