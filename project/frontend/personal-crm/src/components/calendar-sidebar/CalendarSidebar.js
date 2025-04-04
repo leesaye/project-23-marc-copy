87e58812438d8a25f25f3c51625f2e2628482487
lisa <lis.ye@mail.utoronto.ca>
2025-04-04 17:07:09 -0400

    Update BASE URL for env var

--- project/frontend/personal-crm/src/components/calendar-sidebar/CalendarSidebar.js
+++ project/frontend/personal-crm/src/components/calendar-sidebar/CalendarSidebar.js
@@ -5,7 +5,7 @@ import "../../pages/Calendar.css";
 import EventForm from "./EventForm.js";
 import TaskForm from "./TaskForm.js";
 
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export default function CalendarSidebar({
     formType,
